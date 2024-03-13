4ddfa6fa7c2b61be546b240f36c166ba2b1b3e27
Maurits van Altvorst <mvanaltvorst@icloud.com>
2024-03-13 15:49:21 +0000

    Allow for correlation PC factors

--- seminartools/models/pca_var_model.py
+++ seminartools/models/pca_var_model.py
@@ -12,6 +12,12 @@ class PCAVARModel(BaseModel):
     Args:
         country_column (str): The name of the column containing the country names. Used to split the data into countries.
         inflation_column (str): The name of the column containing the inflation rates.
+        num_pcs (int): The number of principal components to use.
+        lags (list[int]): The lags to use in the PCA VAR model.
+        standardize_pre_post (bool): 
+            if True, we standardize the data before fitting the model and revert the standardization after predicting.
+            This way, the covariance we perform PCA on is basically a correlation matrix.
+            This should help with avoiding single countries dominating the PCA.
     """
 
     def __init__(
@@ -20,11 +26,13 @@ class PCAVARModel(BaseModel):
         inflation_column: str = "inflation",
         num_pcs: int = 3,
         lags: list[int] = [1, 2, 3, 4],
+        standardize_pre_post: bool = False,
     ):
         self.country_column = country_column
         self.inflation_column = inflation_column
         self.num_pcs = num_pcs
         self.lags = lags
+        self.standardize_pre_post = standardize_pre_post
 
     def fit(self, data: pd.DataFrame):
         """
@@ -35,6 +43,12 @@ class PCAVARModel(BaseModel):
 
         # Need to store the columns of the wide data for during prediction
         self.data_wide_columns = data_wide.columns
+
+        # Step 0.5: standardize the data
+        if self.standardize_pre_post:
+            self.means = data_wide.mean()
+            self.stds = data_wide.std()
+            data_wide = (data_wide - self.means) / self.stds
 
         # Step 1: factor decomposition
         self.all_eigenvalues, self.all_eigenvectors = np.linalg.eig(data_wide.cov())
@@ -64,6 +78,10 @@ class PCAVARModel(BaseModel):
         data_wide = data.pivot(index="yearmonth", columns="Country", values="inflation")
         data_wide = data_wide[self.data_wide_columns]
 
+        # Step 0.5: standardize the data
+        if self.standardize_pre_post:
+            data_wide = (data_wide - self.means) / self.stds
+
         # Step 1: project the data onto the first num_pcs principal components
         pcs = data_wide.to_numpy() @ self.eigenvectors
         pcs = pd.DataFrame(
@@ -82,6 +100,10 @@ class PCAVARModel(BaseModel):
             index=pc_predictions.index,
             columns=data_wide.columns,
         )
+
+        # Step 3.5: revert the standardization
+        if self.standardize_pre_post:
+            predictions = predictions * self.stds + self.means
 
         # Step 4: convert the predictions back to long format
         predictions = predictions.stack().reset_index().rename(columns={0: "inflation"})
