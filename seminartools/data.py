5a251bcb934f55dd8410d5f891f8bff22fa65939
Maurits van Altvorst <mvanaltvorst@icloud.com>
2024-03-24 15:37:18 +0000

    Read GDP growth data

--- seminartools/data.py
+++ seminartools/data.py
@@ -101,7 +101,13 @@ def read_commodity(
     header_table_1=6,
     header_table_2=9,
     first_difference=True,
-    relevant_variables=['CRUDE_PETRO', 'iNATGAS', 'iAGRICULTURE', 'iMETMIN', 'iPRECIOUSMET']
+    relevant_variables=[
+        "CRUDE_PETRO",
+        "iNATGAS",
+        "iAGRICULTURE",
+        "iMETMIN",
+        "iPRECIOUSMET",
+    ],
 ):
     """
     Reads commodity price data from the CMO-Historical-Data-Monthly.xlsx file and returns a dataframe.
@@ -122,16 +128,91 @@ def read_commodity(
     relevant_variables : list[str]
         List of relevant variables to keep in the dataframe.
     """
-    df_1 = pd.read_excel(filepath, sheet_name=sheet_name_1, header=header_table_1).set_index("Unnamed: 0", drop = True)
-    df_2 = pd.read_excel(filepath, sheet_name=sheet_name_2, header=header_table_2).set_index("Unnamed: 0", drop = True)
-    df = pd.concat([df_1, df_2], axis=1)[relevant_variables] 
-    df = df.apply(pd.to_numeric, errors='coerce').dropna().rename_axis("Date")
-    df.index = pd.to_datetime(df.index, format = "%YM%m")
-    df = df.resample('Q').mean()
+    df_1 = pd.read_excel(
+        filepath, sheet_name=sheet_name_1, header=header_table_1
+    ).set_index("Unnamed: 0", drop=True)
+    df_2 = pd.read_excel(
+        filepath, sheet_name=sheet_name_2, header=header_table_2
+    ).set_index("Unnamed: 0", drop=True)
+    df = pd.concat([df_1, df_2], axis=1)[relevant_variables]
+    df = df.apply(pd.to_numeric, errors="coerce").dropna().rename_axis("Date")
+    df.index = pd.to_datetime(df.index, format="%YM%m")
+    df = df.resample("Q").mean()
     if first_difference:
         df = df.pct_change().dropna()
     return df
 
 
+def read_gdp_growth(
+    *,
+    filepath="./../../assets/GDP-growth.xlsx",
+    sheet_name="Quarterly real GDP growth",
+    header=5,
+    skipfooter=5,
+    add_median=True,
+):
+    """
+    Reads GDP growth data from the GDP-growth.xlsx file and returns a dataframe.
+    This dataframe already contains first differences. There is no option
+    to obtain the raw GDP index.
 
+    Parameters
+    ----------
+    filepath : str
+        Path to the excel file.
+    sheet_name : str
+        Name of the sheet in the excel file.
+    header : int
+        Index of the header of the table.
+    skipfooter : int
+        Number of rows to skip at the end of the table.
+    add_median : bool
+        If True, add a column with the median GDP growth rate across countries.
+    """
+    df = (
+        pd.read_excel(
+            filepath, sheet_name=sheet_name, header=header, skipfooter=skipfooter
+        )
+        .set_index("Period", drop=True)
+        .drop(columns=["Unnamed: 1", "Unnamed: 2"])
+    )
 
+    # Rename columns: only half of the columns are named the respective time period.
+    # We rename the columns to the time period of the previous column.
+
+    for i in range(int(len(df.columns) / 2)):
+        df.rename(columns={df.columns[2 * i + 1]: df.columns[2 * i]}, inplace=True)
+
+    df = df.T
+    df.drop(columns=["Country"], inplace=True)
+
+    # Coerce non-numeric values to NaN
+    df = df.apply(pd.to_numeric, errors="coerce")
+
+    # Delete rows that are all NaN
+    df.dropna(how="all", inplace=True)
+
+    # Change the index to datetime
+    def parse_quarter_string(q_string):
+        # Split the string into its components (e.g., "Q2-1947" -> ["Q2", "1947"])
+        quarter_part, year_part = q_string.split("-")
+
+        # Extract the quarter number
+        quarter_number = int(quarter_part[1])  # Convert "2" to 2
+
+        # Determine the month that corresponds to the quarter
+        month = (
+            quarter_number - 1
+        ) * 3 + 1  # Quarter 1 starts in January, Quarter 2 in April, etc.
+
+        # Create a datetime object for the first day of the starting month of the quarter
+        return pd.Timestamp(year=int(year_part), month=month, day=1)
+
+    # Apply the function to the index
+    df.index = df.index.map(parse_quarter_string)
+
+    if add_median:
+        # Add median column
+        df["median"] = df.median(axis=1)
+
+    return df
