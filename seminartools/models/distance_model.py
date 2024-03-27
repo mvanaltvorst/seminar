9b1ba065c4c1285dfbe7d3f5a8fce2097bdfdd93
Maurits van Altvorst <mvanaltvorst@icloud.com>
2024-03-27 12:26:04 +0000

    Work on distance matrix

--- seminartools/models/distance_model.py
+++ seminartools/models/distance_model.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
-import bambi as bmb
 from .base_model import BaseModel
+from seminartools.utils import geo_distance
 import arviz as az
+import pymc as pm
+from pymc.gp.cov import Matern32
 
 
 class DistanceModel(BaseModel):
@@ -22,6 +24,7 @@ class DistanceModel(BaseModel):
             "commodity_iPRECIOUSMET",
         ],
         tune: int = 500,
+        distance_function: callable = geo_distance,
     ):
         """
         Initializes the model.
@@ -30,6 +33,7 @@ class DistanceModel(BaseModel):
         self.country_column = country_column
         self.target_column = target_column
         self.exogenous_columns = exogenous_columns
+        self.distance_function = distance_function
 
         # Regress on lagged exogenous variables and lagged target variable
         self.lagged_exog_columns = [
@@ -48,8 +52,8 @@ class DistanceModel(BaseModel):
         # Get country out of the multiindex
         data = data.reset_index(level=self.country_column)
 
-        countries = data[self.country_column].unique()
-        if len(countries) <= 1:
+        self.countries = data[self.country_column].unique()
+        if len(self.countries) <= 1:
             print(
                 "WARNING: Only one country in the data in RandomEffectsModel.fit(). Random effects model not applicable."
             )
@@ -61,13 +65,76 @@ class DistanceModel(BaseModel):
             [col in data.columns for col in self.exogenous_columns]
         ), f"Exogenous columns not in data."
 
+        self.distance_matrix = self._geographical_distance_matrix(self.countries)
+
         # Create lagged variables
         data = self._create_lagged_variables(data)
 
         # Drop rows with NaNs due to lagged variables
         data = data.dropna()
 
+        with pm.Model(
+            coords={
+                "country": self.countries,
+                "exogenous": self.exogenous_columns,
+                "target": [self.target_column],
+            }
+        ) as self.model:
+            constant_mean_tau = pm.Gamma("constant_mean_tau", alpha=1, beta=1)
+            constant_mean = pm.Normal("constant_mean", mu=0, tau=constant_mean_tau)
 
+            country_mean_tau = pm.Gamma("country_mean_tau", alpha=1, beta=1)
+            country_means = pm.Normal(
+                "country_means", mu=constant_mean, tau=country_mean_tau, dims="country"
+            )
+
+            regression_coefficient_mean_tau = pm.Gamma(
+                "regression_coefficient_mean_tau", alpha=1, beta=1
+            )
+            regression_coefficient_mean = pm.Normal(
+                "regression_coefficient_mean",
+                mu=0,
+                tau=regression_coefficient_mean_tau,
+                dims="exogenous",
+            )
+
+            regression_coefficient_tau = pm.Gamma(
+                "regression_coefficient_tau", alpha=1, beta=1
+            )
+            regression_coefficients = pm.Normal(
+                "regression_coefficients",
+                mu=regression_coefficient_mean,
+                tau=regression_coefficient_tau,
+                dims=["exogenous", "target"],
+            )
+
+            # Likelihood
+            mu = country_means + pm.math.dot(
+                data[self.exogenous_columns].values, regression_coefficients
+            )
+            sigma = pm.HalfCauchy("sigma", beta=1)
+
+            # New GP part
+            ls = pm.Gamma("ls", alpha=2, beta=1)  # Example length scale prior
+            # cov_func = Matern32Chordal(ls=ls)
+            cov_func = Matern32(2, ls)
+            cov_matrix = cov_func.full_from_distance(self.distance_matrix)
+
+            latent = pm.gp.Latent(cov_func=cov_matrix)
+
+            country_correlated_noise = latent.prior(
+                "country_correlated_noise", dims="country", X=self.countries
+            )
+
+            # Incorporate the GP into your model's likelihood
+            sigma = pm.HalfCauchy("sigma", beta=1)
+            y_obs = pm.Normal(
+                "likelihood",
+                mu=mu + country_correlated_noise,
+                sigma=sigma,
+                observed=data[self.target_column].values,
+                dims="country",
+            )
 
     def _create_lagged_variables(self, data: pd.DataFrame):
         """
@@ -86,6 +153,20 @@ class DistanceModel(BaseModel):
             return country_df
 
         return data.groupby(self.country_column, group_keys=False).apply(_add_lags)
+
+    def _geographical_distance_matrix(self, countries):
+        """
+        Create a matrix of geographical distances between countries based on their coordinates.
+        """
+        distance_matrix = np.zeros((len(countries), len(countries)))
+
+        for i, country_i in enumerate(countries):
+            for j, country_j in enumerate(countries):
+                if i == j:
+                    distance_matrix[i, j] = 0
+                else:
+                    distance_matrix[i, j] = self.distance_function(country_i, country_j)
+        return distance_matrix
 
     def predict(self, data: pd.DataFrame, pointwise_aggregation_method: str = "mean"):
         """
@@ -129,4 +210,4 @@ class DistanceModel(BaseModel):
         #     )
         # )
 
-        return predictions
+        # return predictions
--- seminartools/utils.py
+++ seminartools/utils.py
@@ -2,10 +2,18 @@ from functools import lru_cache
 import math
 import pandas as pd
 import pycountry as pyc
-from seminartools.data import read_gdp
+from .data import read_gdp_level
 
-@lru_cache(maxsize=None)
-def geo_distance(countryA: str, countryB: str) -> float:
+
+DF_DISTANCE_FILEPATH = "./../../assets/dist_cepii.xls"
+DF_DISTANCE = pd.read_excel(DF_DISTANCE_FILEPATH)
+
+
+def geo_distance(
+    countryA: str,
+    countryB: str,
+    country_remap: dict[str, str] = {"Korea": "Korea, Republic of"},
+) -> float:
     """
     Calculate the geographical distance between two countries.
 
@@ -16,18 +24,28 @@ def geo_distance(countryA: str, countryB: str) -> float:
     Returns:
     - float: The geographical distance between the two countries in kilometers.
     """
-    filepath = "./../../assets/dist_cepii.xls"
-    df_distance = pd.read_excel(filepath)
-    countryA = pyc.countries.get(name=countryA)
-    countryB = pyc.countries.get(name=countryB)
+    countryA_remapped = country_remap.get(countryA, countryA)
+    countryB_remapped = country_remap.get(countryB, countryB)
+
+    countryA = pyc.countries.get(name=countryA_remapped)
+    if countryA is None:
+        raise ValueError(f"Country {countryA_remapped} not found.")
+    countryB = pyc.countries.get(name=countryB_remapped)
+    if countryB is None:
+        raise ValueError(f"Country {countryB_remapped} not found.")
 
     # Use the ISO alpha-3 country codes to find the distance.
-    row = df_distance[(df_distance['iso_o'] == countryA.alpha_3) & (df_distance['iso_d'] == countryB.alpha_3)]
-    dist = row['dist'].item()
-    
+    row = DF_DISTANCE[
+        (DF_DISTANCE["iso_o"] == countryA.alpha_3)
+        & (DF_DISTANCE["iso_d"] == countryB.alpha_3)
+    ]
+    dist = row["dist"].item()
+
     return dist
 
-@lru_cache(maxsize=None)
+
+GDP_LEVEL_DF = read_gdp_level()
+
 def gdp(country: str, year: int, quarter: int) -> float:
     """
     Retrieve the GDP of a given country for a specified year and quarter.
@@ -40,13 +58,15 @@ def gdp(country: str, year: int, quarter: int) -> float:
     Returns:
     - float: The GDP of the country for the specified time period.
     """
-    df = read_gdp()
     # Convert quarter to the starting month of the quarter.
     month = quarter * 3 - 2
     time = f"{year}-{month:02d}-01"
-    return df.loc[time, country]
+    return GDP_LEVEL_DF.loc[time, country]
 
-def gravityTradeDistance(countryA: str, countryB: str, year: int, quarter: int) -> float:
+
+def gravityTradeDistance(
+    countryA: str, countryB: str, year: int, quarter: int
+) -> float:
     """
     Estimate trade potential between two countries using a simple gravity model of trade,
     which involves the countries' GDPs and the geographical distance between them.
@@ -63,8 +83,9 @@ def gravityTradeDistance(countryA: str, countryB: str, year: int, quarter: int) 
     GDP_A = gdp(countryA, year, quarter)
     GDP_B = gdp(countryB, year, quarter)
     dist = geo_distance(countryA, countryB)
-    
+
     return GDP_A * GDP_B / dist
+
 
 def proposalDistance(countryA: str, countryB: str, year: int, quarter: int) -> float:
     """
@@ -83,4 +104,11 @@ def proposalDistance(countryA: str, countryB: str, year: int, quarter: int) -> f
     gamma = 0  # Not used in the current formula but could be incorporated later.
     dist = geo_distance(countryA, countryB) + theta
     # Adjust distance calculation based on GDP differences.
-    return dist / (math.log(gdp(countryA, year, quarter)) - math.log(gdp(countryB, year, quarter)))**2
+    return (
+        dist
+        / (
+            math.log(gdp(countryA, year, quarter))
+            - math.log(gdp(countryB, year, quarter))
+        )
+        ** 2
+    )
